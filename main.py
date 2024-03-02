78cc05b97b452f31c0072b321417441520dcd8cd
adam.pentkowski124@gmail.com <adam.pentkowski124@gmail.com>
2024-03-02 20:30:08 +0100

    add background tilemap

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sprite_manager as sm
 import logging
 from entity import Entity
 import camera
+import random 
+import numpy as np
 
 import pygame
 from pygame_gui import *
@@ -18,7 +20,19 @@ logging.basicConfig(
 )
 
 spr = None
+bg_tile_map = [None]*256*256
+bg_tiles = None
 
+def generate_tilemap():
+    global bg_tile_map
+    for y in range(0, 256):
+        for x in range(0, 256):
+            bg_tile_map[y*256 + x] = (1,1)
+            rnd = random.randrange(0, 100)
+            if rnd < 12:
+                bg_tile_map[y*256 + x] = (rnd - 6, 5)
+                if rnd < 6:
+                    bg_tile_map[y*256 + x] = (rnd, 6)
 
 
 def update(delta_time):
@@ -35,6 +49,17 @@ def update(delta_time):
     ui_refresh -= 1
 
 def render():
+    global bg_tile_map, bg_tiles, spr
+    tile_draw_range = (int(camera.get_pos()[0] // 32), int(camera.get_pos()[1] // 32))
+    for y in range(tile_draw_range[1], tile_draw_range[1] + 16):
+        for x in range(tile_draw_range[0], tile_draw_range[0] + 16):
+            bg_tiles = sm.get_sprite("Grass.png", bg_tile_map[y*256 + x])
+            bg_tiles.set_pos(x * 32, y * 32)
+            bg_tiles.draw()
+
+        
+    
+
     spr.draw()
     spr.draw_collision_rect()
 
@@ -48,9 +73,14 @@ ui_frame_timer = UILabel(pygame.Rect(10,32,200,24),"Frame time: 0", pw.get_ui_ma
 ui_camera_pos = UILabel(pygame.Rect(10,48,200,24),f"Camera: {camera.get_pos()}", pw.get_ui_manager(),object_id='#camera')
 ui_refresh = pw.get_fps()
 
+generate_tilemap()
+
 spr = Entity(sm.get_sprite("bird.png"), 2700, 2700)
 #spr.set_pos(2600, 2600)
 spr.resize(64, 64)
+
+sm.load_image_tiles("Grass.png", 16, 11, 7, 32)
+
 
 pw.loop(update, render)
 
--- sprite_manager.py
+++ sprite_manager.py
@@ -16,13 +16,36 @@ def load_image(path) -> None:
         img = img.convert_alpha()
         images[path] = img
 
-def get_image(path) -> pygame.Surface:
+def load_image_tiles(path, orginal_tile_size, num_tiles_x, num_tiles_y, tile_size) -> None:
+    img = pw.get_pygame().image.load(path)
+    if img == None:
+        log.error("Failed to load image: " + path)
+    else:
+        log.info("Loaded image: " + path)
+        img.set_colorkey((255,0,255))
+        img = img.convert_alpha()
+        for x in range(num_tiles_x):
+            for y in range(num_tiles_y):
+                images[path + "_" + str(x) + "_" + str(y)] = pw.get_pygame().transform.scale(img.subsurface(x * orginal_tile_size, y * orginal_tile_size, orginal_tile_size, orginal_tile_size), (tile_size, tile_size))
+
+    
+
+def get_image(path, tile = (-1, -1)) -> pygame.Surface:
+
+    if tile[0] != -1 and tile[1] != -1:
+        try:
+            tmp = images[path + "_" + str(tile[0]) + "_" + str(tile[1])]
+        except:
+            log.error("Failed to get image: " + path + "_" + str(tile[0]) + "_" + str(tile[1]) + " tile not found")
+            return None
+        return tmp
+    
     if images.get(path) == None:
         load_image(path)
     return images[path]
 
-def get_sprite(path) -> Sprite:
-    return Sprite(get_image(path))
+def get_sprite(path, tile = (-1, -1)) -> Sprite:
+    return Sprite(get_image(path, tile))
 
 
 def get_images() -> dict:
